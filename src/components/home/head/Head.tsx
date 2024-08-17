d082a4b63e8aa2a53aeb578f6aa0b1f841584f1f
admin1334 <jeetbherwani2004@gmail.com>
2024-08-17 23:28:20 +0530

    minor add

--- src/components/home/head/Head.tsx
+++ src/components/home/head/Head.tsx
@@ -15,7 +15,7 @@ function Head(): React.ReactElement {
             <p className="opacity-60 text-xs md:text-sm">Upload your files and keep it safe on blockchain</p>
             
             {pinataContext && !pinataContext?.account && <div className="w-full flex flex-col items-center justify-center mt-10">
-                <p className="opacity-80">Connect your <span className="font-semibold text-orange-500">MetaMask</span> to view and upload files. Change network to Ethereum <span className='text-pink-500'>Sepolia</span> testnet of your wallet</p>
+                <p className="w-[80%] text-center opacity-80">Connect your <span className="font-semibold text-orange-500">MetaMask</span> to view and upload files. Change network to Ethereum <span className='text-pink-500'>Sepolia</span> testnet of your wallet</p>
                 <button onClick={handleConnectWeb3} className='text-xs md:text-sm hover:bg-transparent hover:text-gray-800 my-3 md:my-2 px-6 py-3 rounded-3xl border-[0.5px] border-orange-600 bg-orange-600 text-white shadow-lg dark:bg-[#151515] dark:hover:bg-orange-600 transition-all duration-200 font-semibold'>Connect Wallet</button>
             </div>}
         </div>
